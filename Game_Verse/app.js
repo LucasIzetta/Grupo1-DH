5a8effae0dc142021d093a73c91b443f8c980a6d
Nataniel Cazon <natanielarg@gmail.com>
2024-01-11 11:19:18 -0300

    Desarrollo del navbar

--- Game_Verse/app.js
+++ Game_Verse/app.js
@@ -15,3 +15,7 @@ app.listen(port, () => {
 app.get(`/`, (req, res) => {
   res.sendFile(path.join(__dirname, `/views/index.html`))
 });
+
+app.get(`/navbar`, (req, res) => {
+  res.sendFile(path.join(__dirname, `/views/navbar.html`))
+});
